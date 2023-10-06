89c4d53f725c37e0425720fa78e89cf4f1caf359
Elvis Zhune <elfabri28@gmail.com>
2023-10-06 11:19:18 +0200

    Practica Django hecha

--- Animales/views.py
+++ Animales/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def animal_list(request):
+    return render(request, 'animales/animal_list.html', {})
+def protectora_list(request):
+    return render(request, 'animales/protectora_list.html', {})
+def colaborador_list(request):
+    return render(request, 'animales/colaborador_list.html', {})
